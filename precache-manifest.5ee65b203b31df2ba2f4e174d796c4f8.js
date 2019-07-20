1761f396fc252d2babe88adf63b0648ba3c916c8
eravinashm <avinash532110@gmail.com>
2019-07-20 18:38:36 +0530

    Updates

---
+++ precache-manifest.5ee65b203b31df2ba2f4e174d796c4f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61c8833258f299388097",
+    "url": "/eravinashm/weather-app/static/js/runtime~main.b5defe1f.js"
+  },
+  {
+    "revision": "d7c4dc16d1b1c1e5e50e",
+    "url": "/eravinashm/weather-app/static/js/main.4f9eeaee.chunk.js"
+  },
+  {
+    "revision": "4a0f86b52b5b631bf560",
+    "url": "/eravinashm/weather-app/static/js/2.5f59efdd.chunk.js"
+  },
+  {
+    "revision": "d7c4dc16d1b1c1e5e50e",
+    "url": "/eravinashm/weather-app/static/css/main.2e486db4.chunk.css"
+  },
+  {
+    "revision": "e51add24aedf6029c19d2860f4b49551",
+    "url": "/eravinashm/weather-app/index.html"
+  }
+];
--- src/constants/Credentials.js
+++
@@ -1,2 +0,0 @@
-// http://api.openweathermap.org rest api
-export const APP_ID = '09fc2fbe71c94cb399114e42d35996f1';
