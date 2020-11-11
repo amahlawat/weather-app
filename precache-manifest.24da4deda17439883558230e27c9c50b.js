368b891a41622f47dddd1e196440e782936f9edf
amahlawat <avinash532110@gmail.com>
2020-11-11 20:34:53 +0530

    Updates

---
+++ precache-manifest.24da4deda17439883558230e27c9c50b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "acff765d3c5602248ccb",
+    "url": "/weather-app/static/css/main.bd8be1ba.chunk.css"
+  },
+  {
+    "revision": "acff765d3c5602248ccb",
+    "url": "/weather-app/static/js/main.1c704507.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "4f38e1235d8acdc320be",
+    "url": "/weather-app/static/js/2.c0c344fb.chunk.js"
+  },
+  {
+    "revision": "3d872e4e56fb33ccb9943204604a0536",
+    "url": "/weather-app/static/media/weather.3d872e4e.gif"
+  },
+  {
+    "revision": "05208ecdda2f378e7132ccc583e5653f",
+    "url": "/weather-app/index.html"
+  }
+];
--- src/components/Loader/ClimbingBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { css } from '@emotion/core';
-import { ClimbingBoxLoader } from 'react-spinners';
- 
-const ClimbingBox = (props) => (
-    <ClimbingBoxLoader size={30} />
-)
-
-export default ClimbingBox;
